3d0c1e4a66bbc6999b89e83e5cd236cffe604168
jolly <jolly@jollydeMacBook-Pro.local>
2019-02-15 23:35:21 +0800

    修改注释

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # XibInHerit
+### 1. loader+UIView处理了xib创建的视图类的继承关系和事件透传屏蔽相关。如demo里面的xib继承关系链:SubView<SecView<BaseView。
+### 2. xib加载到storyboard中使用，参照MyView的实现。
+### 3. 效果如下:
+![](./shot.png)
--- XIbInHerit/ViewInHerit/loader+UIView.h
+++ XIbInHerit/ViewInHerit/loader+UIView.h
@@ -12,7 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIView(loader)
 
-@property(nonatomic,assign)BOOL touchPassThrough;//事件透传
+@property(nonatomic,assign)BOOL touchPassThrough;//若该值为YES则忽略该视图的响应事件，传递给下层视图处理。
 + (id)nib_load:(Class)cla;
 - (void)iv_setOrigin:(CGPoint )pt;
 - (void)iv_setSize:(CGSize )size;
